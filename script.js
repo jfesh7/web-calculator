ad3017ca2203d39e4ceaef1db67f767dd2f6cad1
Josiah Fesh <josiahdfesh@gmail.com>
2022-12-23 23:21:42 -0500

    Add basic calculator functionality
    The calculator is configured using an expression object to keep
    track of operands and operators. The numbers are stored as strings
    for display and concatenation and converted to numbers for
    operations. Zero cannot be entered after a divide. Expression does
    not evaluation without a secondary field in expression. Code needs
    further cleanup and simplification.

--- script.js
+++ script.js
@@ -1,3 +1,86 @@
 //Calculator functions
 
-console.log("Hello World");
+function calculate(x, y, operator) {
+	let result = x;
+	if (!Number(x) && !Number(y)) {
+		return 'Error';
+	}
+	if (!['+', '-', '*', '/'].includes(operator)) {
+		return 'Invalid operation';
+	}
+
+	switch (operator) {
+	case '+':
+		result = x + y;
+		break;
+	case '-':
+		result = x - y;
+		break;
+	case '*':
+		result = x * y;
+		break;
+	case '/':
+		result = x / y;
+		break;
+	}
+
+	return result;
+}
+
+function evaluate(event, expression) {
+	if (expression.secondary) {
+		expression.main = calculate(Number(expression.main), Number(expression.secondary), expression.operator);
+	}
+	expression.secondary = '';
+	expression.operator = '';
+}
+
+function storeOperation(event, expression) {
+	if (expression.operator) {
+		evaluate(event, expression);
+	}
+	expression.operator = event.target.value;
+}
+
+function storeNumber(event, expression) {
+	if (!expression.operator) {
+		expression.main += event.target.value;
+	}
+	else {
+		if (!(expression.operator === '/' && event.target.value === '0')) {
+			expression.secondary += event.target.value;	
+		}
+	}
+	console.table(expression);
+}
+
+function updateDisplay(display, expression) {
+	display.value = `${expression.main} ${expression.operator} ${expression.secondary}`;
+}
+
+
+//Calculator components
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operator');
+const equalButton = document.querySelector('#evaluate');
+const display = document.querySelector('#display');
+let currentExpression = {
+	'main': '',
+	'secondary': '',
+	'operator': '',
+};
+
+numberButtons.forEach(button => button.addEventListener('click', (event) => {
+	storeNumber(event, currentExpression);
+	updateDisplay(display, currentExpression);
+}));
+
+operatorButtons.forEach(button => button.addEventListener('click', (event) => {
+	storeOperation(event, currentExpression);
+	updateDisplay(display, currentExpression);
+}));
+
+equalButton.addEventListener('click', (event) => {
+	evaluate(event, currentExpression);
+	updateDisplay(display, currentExpression);
+});
